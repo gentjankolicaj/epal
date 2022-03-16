8edf588ca081b2551aa96efe87f82dcb06c1ce26
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-03-16 23:01:58 +0100

    reformat code.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

--- image-service/src/main/java/org/epal/image_service/controller/HomeController.java
+++ image-service/src/main/java/org/epal/image_service/controller/HomeController.java
@@ -3,6 +3,7 @@ package org.epal.image_service.controller;
 import org.epal.image_service.domain.Image;
 import org.epal.image_service.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/image")
 public class HomeController {
 
     private final ImageService imageService;
@@ -20,11 +21,13 @@ public class HomeController {
         this.imageService = imageService;
     }
 
+    @GetMapping
     @RequestMapping("/bus")
     public List<Image> getBusImages() {
         return imageService.findAllImages();
     }
 
+    @GetMapping
     @RequestMapping("/bus/{busId}")
     public List<Image> getBusImage(@PathVariable("busId") Long busId) {
         return imageService.findImagesByBusId(busId);
