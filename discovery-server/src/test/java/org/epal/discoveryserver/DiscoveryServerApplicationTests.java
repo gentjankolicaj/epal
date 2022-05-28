8ec81416751027c4a5ddeed570d83be4b3617e7d
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-05-28 22:01:11 +0200

    Added maven surefire plugin for test reports (xml format).

--- discovery-server/src/test/java/org/epal/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/org/epal/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,9 +1,7 @@
 package org.epal.discoveryserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DiscoveryServerApplicationTests {
 
     @Test
